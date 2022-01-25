e692dd11fe16025a9aed159d22da068aa688e08a
avm <avm@ryerson.ca>
2022-01-24 22:06:05 -0500

    Add sample Slack bot

---
+++ 1_slack_bot/README.md
@@ -0,0 +1,9 @@
+# Sample Slack Bot
+Let's explore Events API. 
+* The tutorial is loosely based on these two tutorials 
+  [[1](https://github.com/slackapi/python-slack-sdk/tree/main/tutorial),
+  [2](https://api.slack.com/start/building/bolt-python)].
+* Create your own bot message using [Block Kit Builder](https://app.slack.com/block-kit-builder/).
+* Keep in mind that you will need a public URL to interact with the bot (if you are not using Socket Mode). 
+  Slack recommends using [ngrok](https://ngrok.com).
+
---
+++ 1_slack_bot/app.py
@@ -0,0 +1,101 @@
+import logging
+from slack_bolt import App
+
+# Initialize a Bolt for Python app
+app = App()
+
+
+def send_msg(client, msg):
+    """
+    Send out the message
+
+    :param client: slack client
+    :param msg: the message in slack format
+    :return: none
+    """
+    response = client.chat_postMessage(**msg)
+    logger.info(f"Send the message at {response['ts']}")
+
+
+def unknown_cmd(user, channel, client):
+    """
+    Echo the command back
+
+    :param user: user id
+    :param channel: channel id
+    :param client: slack client
+    :param text: text of the message
+    :return: none
+    """
+    msg = {
+        "ts": "",
+        "channel": channel,
+        "username": user,
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"Hello, *{user}*. I do not know how to process your command, try adding `echo`."
+                }
+            }
+        ],
+    }
+
+    send_msg(client, msg)
+
+
+def echo_cmd(user, channel, client, text):
+    """
+    Echo the command back
+
+    :param user: user id
+    :param channel: channel id
+    :param client: slack client
+    :param text: text of the message
+    :return: none
+    """
+    msg = {
+        "ts": "",
+        "channel": channel,
+        "username": user,
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"Hello, *{user}*. Thanks for your message, let me echo it back to you:\n"
+                            f"> {text}"
+                }
+            }
+        ],
+    }
+
+    send_msg(client, msg)
+
+
+# when bot name is mentioned
+@app.event("app_mention")
+def message(event, client):
+    """
+
+    :param event: event payload
+    :param client: slack client
+    :return: none
+    """
+
+    channel_id = event.get("channel")
+    user_id = event.get("user")
+    text = event.get("text")
+
+    if text and "echo" in text:
+        return echo_cmd(user_id, channel_id, client, text)
+    else:
+        return unknown_cmd(user_id, channel_id, client, text)
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler())
+    app.start(3000)  # port 3000
